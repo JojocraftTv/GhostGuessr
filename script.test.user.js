7247e0eb7588c50d897731aca35ce7dfc407dd1e
Niclas <105665457+nici002018@users.noreply.github.com>
2026-01-17 21:56:36 +0100

    feat: Add initial UserScript for GhostGuessr with essential metadata and requirements

---
+++ script.test.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         GhostGuessr (Test)
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Marks your Current Streetview Location on the GeoGuessr Map with a Red Dot. Toggle with hotkey.
+// @author       VellusFox, Niceas
+// @match        https://www.geoguessr.com/*
+// @icon         https://raw.githubusercontent.com/JojocraftTv/GhostGuessr/refs/heads/test/icons/GhostGuessr.png
+// @require      https://raw.githubusercontent.com/JojocraftTv/GhostGuessr/refs/heads/test/scripts/ghostguessr-state.js
+// @require      https://raw.githubusercontent.com/JojocraftTv/GhostGuessr/refs/heads/test/scripts/ghostguessr-utils.js
+// @require      https://raw.githubusercontent.com/JojocraftTv/GhostGuessr/refs/heads/test/scripts/ghostguessr-map.js
+// @require      https://raw.githubusercontent.com/JojocraftTv/GhostGuessr/refs/heads/test/scripts/ghostguessr-ui.js
+// @require      https://raw.githubusercontent.com/JojocraftTv/GhostGuessr/refs/heads/test/scripts/ghostguessr-settings.js
+// @require      https://raw.githubusercontent.com/JojocraftTv/GhostGuessr/refs/heads/test/scripts/ghostguessr-main.js
+// @downloadURL  https://raw.githubusercontent.com/JojocraftTv/GhostGuessr/refs/heads/test/script.user.test.js
+// @updateURL    https://raw.githubusercontent.com/JojocraftTv/GhostGuessr/refs/heads/test/script.user.test.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @license      MIT
+// ==/UserScript==
